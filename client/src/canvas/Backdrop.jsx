b17a408673bb77571af7cfda97cc4df5663aec7a
AFFAN AHMED GHOURI <notjerry22@gmail.com>
2026-04-27 02:03:24 +0500

    Fix: Typo in Backdrop, improve 3D rendering

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react'
-import { easing } from 'maath'
 import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
@@ -12,7 +11,7 @@ const Backdrop = () => {
       temporal
       frames={60}
       alphaTest={0.85}
-      scae={0.5}
+      scale={0.5}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
