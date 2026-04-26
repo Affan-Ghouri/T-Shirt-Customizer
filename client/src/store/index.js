35cceddad222023170503836ea962084029c61f2
AFFAN AHMED GHOURI <notjerry22@gmail.com>
2026-04-27 01:26:42 +0500

    Fix: Asset paths for GitHub Pages deployment

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: '/T-Shirt-Customizer/threejs.png',
+  fullDecal: '/T-Shirt-Customizer/threejs.png',
   isAIEnabled: false,
 });
 
